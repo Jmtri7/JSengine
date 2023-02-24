138a23aa62f0291e2be9a7245503daf27af4d3b5
Jmtri7 <Jmtri7@gmail.com>
2023-02-24 11:49:11 -0500

    Update Game.js

--- docs/Game.js
+++ docs/Game.js
@@ -1,26 +1,52 @@
+class Rig extends Edge {
+	constructor(p1, p2) {
+		super(p1, p2);
+		this.children = [];
+	}
+	extrude(p3) {
+		let r2 = new Rig(this.p2, p3);
+		this.children.push(r2);
+		return r2;
+	}
+	rotate(center, angle) {
+		this.p1.rotate(center, angle);
+		this.p2.rotate(center, angle);
+		for(var i = 0; i < this.children.length; i++) {
+			this.children[i].rotateChild(center, angle);
+		}
+		return this;
+	}
+	rotateChild(center, angle) {
+		this.p2.rotate(center, angle);
+		for(var i = 0; i < this.children.length; i++) {
+			this.children[i].rotateChild(center, angle);
+		}
+	}
+	render(renderer) {
+		renderer.DrawEdge(this);
+		for(var i = 0; i < this.children.length; i++) {
+			this.children[i].render(renderer);
+		}
+	}
+}
 class Game extends CanvasEngine {
 	constructor(windowWidth, windowHeight, speed) {
 		super(windowWidth, windowHeight, speed);
 
-		this.p1 = new Point(0, 0);
-		this.p2 = new Point(100, 0);
-		this.p3 = new Point(100, 50);
-
-		this.e1 = new Edge(this.p1, this.p2);
-		this.e2 = new Edge(this.p2, this.p3);
+		this.rig = new Rig(new Point(0, 0), new Point(100, 0))
+		let r1 = this.rig.extrude(new Point(100, 50))
+		let r2 = this.rig.extrude(new Point(100, -50))
+		let r3 = this.rig.extrude(new Point(150, 0))
 	}
 	Update(dt) {
-		this.p2.rotate(this.p1, dt / 16);
-		this.p3.rotate(this.p1, dt / 16);
-		this.p3.rotate(this.p2, dt / 16 * 2);
+		this.rig.rotate(this.rig.p1, dt / 16);
 	}
 	Render() {
 		this.Clear();
 		this.DrawAxes();
 		this.PaintRectangle(this.cursor.x - 5, this.cursor.y - 5, 10, 10, "red");
 
-		this.DrawEdge(this.e1);
-		this.DrawEdge(this.e2);
+		this.rig.render(this);
 		
 	}
 	DrawEdge(edge) {
